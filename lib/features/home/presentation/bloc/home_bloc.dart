9c15a9c5d6be69bfd119875c997f64de3cc69aef
nahian0 <nahianpathan12@gmail.com>
2023-02-23 17:28:12 +0600

    Bloc implemented for homepage

--- lib/features/home/presentation/bloc/home_bloc.dart
+++ lib/features/home/presentation/bloc/home_bloc.dart
@@ -1,13 +1,35 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:myapp/core/di/app_component.dart';
+import 'package:myapp/features/home/data/models/NowPlayingModel.dart';
+import 'package:myapp/features/home/data/models/PopularModel.dart';
+import 'package:myapp/features/home/domain/repositories/Home_page_Repositorie.dart';
+import 'package:myapp/features/home/domain/usecases/NowplayinUsecase.dart';
+import 'package:myapp/features/home/domain/usecases/Popularmoviesusecase.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
+  // ignore: prefer_final_fields
+  NowplayingUsecase _nowplayingUsecase =
+      NowplayingUsecase(locator<HomePageRepositories>());
+
+  // ignore: prefer_final_fields
+  PopularMovieUsecase _popularMovieUsecase =
+      PopularMovieUsecase(locator<HomePageRepositories>());
+
+  HomeBloc(this._nowplayingUsecase, this._popularMovieUsecase)
+      : super(dataLoadingState()) {
+    on<LoadHomeEvent>((event, emit) async {
+      emit(dataLoadingState());
+      try {
+        var a = await _nowplayingUsecase(page: 1);
+        var b = await _popularMovieUsecase(page: 1);
+        emit(dataLoadedState(a, b));
+
+        // ignore: empty_catches
+      } catch (e) {}
     });
   }
 }
